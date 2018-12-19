ac1a2030fcdd70677a653bb0b1074eb7da70e04d
gaesar1 <601998354@qq.com>
2018-12-19 13:37:04 +0800

    Update cloud.php

--- src/cloud.php
+++ src/cloud.php
@@ -69,24 +69,7 @@ Cloud::define("resetscore", function() {
 
 });
 Cloud::define("averageStars", function($params, $user) {
-    $query = new Query("Review");
-    $query->equalTo("movie", $params["movie"]);
-    try {
-        $reviews = $query->find();
-    } catch (CloudException $ex) {
-        // 查询失败, 将错误输出到日志
-        error_log($ex->getMessage());
-        return 0;
-    }
-    $sum = 0;
-    forEach($reviews as $review) {
-        $sum += $review->get("stars");
-    }
-    if (count($reviews) > 0) {
-         return $sum / count($reviews);
-    } else {
-         return 0;
-    }
+error_log("执行averageStars");
 });
 
 
